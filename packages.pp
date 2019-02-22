b2e930bf570c65e753abd56a759e5e565096eedc
Baztronic <Baztronic@users.noreply.github.com>
2019-02-22 10:17:29 +0000

    Duh formatting

--- packages.pp
+++ packages.pp
@@ -2,88 +2,215 @@ package
   {
   'epel-release':
   ensure => installed,
+}
+package
+{
   'sendmail-cf':
   ensure => installed,
+}
+package
+{
   'sendmail':
   ensure => installed,
+}
+package
+{
   'logwatch':
   ensure => installed,
+}
+package
+{
   'screen':
   ensure => installed,
+}
+package
+{
   'policycoreutils-python':
   ensure => installed,
+}
+package
+{
   'mlocate':
   ensure => installed,
+}
+package
+{
   'perl':
   ensure => installed,
+}
+package
+{
   'yum-cron':
   ensure => installed,
+}
+package
+{
   'nfs':
   ensure => installed,
+}
+package
+{
   'bind-utils':
   ensure => installed,
+}
+package
+{
   'nfs-utils':
   ensure => installed,
+}
+package
+{
   'net-tools':
   ensure => installed,
+}
+package
+{
   'sysstat':
   ensure => installed,
+}
+package
+{
   'net-snmp':
   ensure => installed,
+}
+package
+{
   'deltarpm':
   ensure => installed,
+}
+package
+{
   'samba-winbind-modules*':
   ensure => installed,
+}
+package
+{
   'wget':
   ensure => installed,
+}
+package
+{
   'htop':
   ensure => installed,
+}
+package
+{
   'samba-client':
   ensure => installed,
+}
+package
+{
   'cifs-utils':
   ensure => installed,
+}
+package
+{
   'samba-common-tools':
   ensure => installed,
+}
+package
+{
   'nscd':
   ensure => installed,
+}
+package
+{
   'ntp':
   ensure => installed,
+}
+package
+{
   'iptables-services':
   ensure => installed,
+}
+package
+{
   'snmpd':
   ensure => installed,
+}
+package
+{
   'mpich-3.0-devel-*':
   ensure => installed,
+}
+package
+{
   'libpng':
   ensure => installed,
+}
+package
+{
   'netcdf':
   ensure => installed,
+}
+package
+{
   'netcdf-devel':
   ensure => installed,
+}
+package
+{
   'm4':
   ensure => installed,
+}
+package
+{
   'mpich':
   ensure => installed,
+}
+package
+{
   'gcc':
   ensure => installed,
+}
+package
+{
   'cpp':
   ensure => installed,
+}
+package
+{
   'gcc-gfortran':
   ensure => installed,
+}
+package
+{
   'gcc-c++':
   ensure => installed,
+}
+package
+{
   'jasper*':
   ensure => installed,
+}
+package
+{
   'zlib*':
   ensure => installed,
+}
+package
+{
   'netcdf*':
   ensure => installed,
+}
+package
+{
   'libpng*':
   ensure => installed,
+}
+package
+{
   'hdf':
   ensure => installed,
+}
+package
+{
   'hdf-devel':
   ensure => installed,
+}
+package
+{
   'ncdu':
   ensure => installed,  
   }
+
