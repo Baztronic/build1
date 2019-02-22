4ea3bb51df435f72ca2ea37269f2018191d9e363
Baztronic <Baztronic@users.noreply.github.com>
2019-02-22 10:30:37 +0000

    Update packages.pp
    Put deltarpm first

--- packages.pp
+++ packages.pp
@@ -1,4 +1,4 @@
-$needed=['sendmail-cf','ncdu','sendmail','logwatch','screen','policycoreutils-python','mlocate','perl','yum-cron','nfs','bind-utils','nfs-utils','net-tools','sysstat','net-snmp','deltarpm','samba-winbind-modules*','wget','htop','samba-client','cifs-utils','samba-common-tools','nscd','ntp','iptables-services','snmpd','mpich-3.0-devel-*','libpng','netcdf','netcdf-devel','m4','mpich','gcc','cpp','gcc-gfortran','gcc-c++','jasper*','zlib*','netcdf*','libpng*','hdf','hdf-devel']
+$needed=['deltarpm','sendmail-cf','ncdu','sendmail','logwatch','screen','policycoreutils-python','mlocate','perl','yum-cron','nfs','bind-utils','nfs-utils','net-tools','sysstat','net-snmp','samba-winbind-modules*','wget','htop','samba-client','cifs-utils','samba-common-tools','nscd','ntp','iptables-services','snmpd','mpich-3.0-devel-*','libpng','netcdf','netcdf-devel','m4','mpich','gcc','cpp','gcc-gfortran','gcc-c++','jasper*','zlib*','netcdf*','libpng*','hdf','hdf-devel']
 package
   {
   'epel-release':
