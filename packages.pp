20f94b0934a230d7f3204bde2476bb5fe7c84aa4
Baztronic <Baztronic@users.noreply.github.com>
2019-02-22 10:14:23 +0000

    Create packages.pp
    Package install test

---
+++ packages.pp
@@ -0,0 +1,89 @@
+package
+  {
+  'epel-release':
+  ensure => installed,
+  'sendmail-cf':
+  ensure => installed,
+  'sendmail':
+  ensure => installed,
+  'logwatch':
+  ensure => installed,
+  'screen':
+  ensure => installed,
+  'policycoreutils-python':
+  ensure => installed,
+  'mlocate':
+  ensure => installed,
+  'perl':
+  ensure => installed,
+  'yum-cron':
+  ensure => installed,
+  'nfs':
+  ensure => installed,
+  'bind-utils':
+  ensure => installed,
+  'nfs-utils':
+  ensure => installed,
+  'net-tools':
+  ensure => installed,
+  'sysstat':
+  ensure => installed,
+  'net-snmp':
+  ensure => installed,
+  'deltarpm':
+  ensure => installed,
+  'samba-winbind-modules*':
+  ensure => installed,
+  'wget':
+  ensure => installed,
+  'htop':
+  ensure => installed,
+  'samba-client':
+  ensure => installed,
+  'cifs-utils':
+  ensure => installed,
+  'samba-common-tools':
+  ensure => installed,
+  'nscd':
+  ensure => installed,
+  'ntp':
+  ensure => installed,
+  'iptables-services':
+  ensure => installed,
+  'snmpd':
+  ensure => installed,
+  'mpich-3.0-devel-*':
+  ensure => installed,
+  'libpng':
+  ensure => installed,
+  'netcdf':
+  ensure => installed,
+  'netcdf-devel':
+  ensure => installed,
+  'm4':
+  ensure => installed,
+  'mpich':
+  ensure => installed,
+  'gcc':
+  ensure => installed,
+  'cpp':
+  ensure => installed,
+  'gcc-gfortran':
+  ensure => installed,
+  'gcc-c++':
+  ensure => installed,
+  'jasper*':
+  ensure => installed,
+  'zlib*':
+  ensure => installed,
+  'netcdf*':
+  ensure => installed,
+  'libpng*':
+  ensure => installed,
+  'hdf':
+  ensure => installed,
+  'hdf-devel':
+  ensure => installed,
+  'ncdu':
+  ensure => installed,  
+  }
